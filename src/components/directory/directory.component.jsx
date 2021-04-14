c23f66be62c1a76e098d96144c20b6f782b72f44
Nishant Kumar <nishantcafe@gmail.com>
2021-04-14 23:23:22 +0530

    adding hover effect

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -47,8 +47,8 @@ class Directory extends React.Component {
         return (
             <div className="directory-menu">
                 {
-                    this.state.sections.map(({title,id,imageUrl}) => (
-                        <MenuItem key={id} title={title} imageUrl={imageUrl} />
+                    this.state.sections.map(({title,id,imageUrl,size}) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
                     ))
                 }
             </div>
